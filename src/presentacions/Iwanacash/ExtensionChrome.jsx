14edd3a542719a8ac03c08d28763455edea81ced
teremir <axlina90@gmail.com>
2022-03-27 20:02:53 -0400

    Anexando vista de extension

--- src/presentacions/Iwanacash/ExtensionChrome.jsx
+++ src/presentacions/Iwanacash/ExtensionChrome.jsx
@@ -1,5 +1,6 @@
 import Layout from "../../components/iwana-cash/Layout/Layout"
 import group_extension from "../../assets/images/iwana-cash/group_extension.png" 
+import group_extension_dsk from "../../assets/images/iwana-cash/group_extension_dsk.png" 
 import icon_google from "../../assets/images/iwana-cash/icon-google.png" 
 import img_work_1 from "../../assets/images/iwana-cash/ext-work.png" 
 import img_work_dsk_1 from "../../assets/images/iwana-cash/ext-work-dsk.png" 
@@ -14,17 +15,18 @@ const ExtensionChrome = () => {
 		<Layout>
 			<div className="row">
 				<div className="col-12 col-main mb-5">
-					<section className="store-detail">
+					<section className="store-extension_detail">
 						<div className="store-header">
-							<p className="title-h2 title-store">Nuestra Extensión</p>
-							<p className="store-h4 mt-4">Instala nuestra extensión en tu navegador y comienza a ganar dinero en tus compras diarias.</p>
-							<div className="clic-button">
+							<p className="title-h2 title-store mb-0">Nuestra </p>
+							<p className="title-h2 title-store">Extensión</p>
+							<p className="store-h4 extension-main">Instala nuestra extensión en tu navegador y comienza a ganar dinero en tus compras diarias.</p>
+							<div className="clic-button clic-button__extension">
 								<img src={icon_google} alt="Icono Google Chrome" />
 								<button type="button" className="btn btn-blue btn-extension">instalar extensión</button>
 							</div>
 						</div>
-						<div className="clic-background">
-							<img src={group_extension} alt="Instalar extensión Google" className="clic-background__image" />
+						<div className="clic-background clic-background__extension">
+							<img src={group_extension_dsk} alt="Instalar extensión Google" className="clic-background__image" />
 						</div>
 					</section>
 				</div>
@@ -40,7 +42,7 @@ const ExtensionChrome = () => {
 			</div>
 			<div className="row row-extension">
 				<div className="col-12">
-					<div className="container extension-panel">
+					<div className="container extension-panel p-extension">
 						<section className="work-main pr-md-5">
 							<div className="clic-title ext-clic-title mt-md-0">
 								<p>Cómo funciona</p>
@@ -59,14 +61,14 @@ const ExtensionChrome = () => {
 					</div>
 				</div>
 				<div className="col-12">
-					<div className="container extension-cashback">
+					<div className="container extension-cashback p-extension">
 						<section className="work-main text-md-right pl-md-5">
 							<div className="clic-title ext-clic-title mt-4">
 								<p>Activa tu cashback</p>
 							</div>
 							<div>
-								<p className="store-h4 text-right mt-4 mb-0">Cientos de tiendas disponibles.</p>
-								<p className="extension-h4 text-right">La instalación es rápida, sencilla y confiable. En tan solo 1 minuto puedes instalar la extensión para Chrome y así podrás activar el cashback directamente en cada tienda, sin necesidad de pasar por la plataforma iwanacash.com</p>
+								<p className="store-h4 text-md-right mt-4 mb-0">Cientos de tiendas disponibles.</p>
+								<p className="extension-h4 text-md-right">La instalación es rápida, sencilla y confiable. En tan solo 1 minuto puedes instalar la extensión para Chrome y así podrás activar el cashback directamente en cada tienda, sin necesidad de pasar por la plataforma iwanacash.com</p>
 							</div>
 						</section>
 						<section className="work-secondary">
@@ -76,8 +78,8 @@ const ExtensionChrome = () => {
 						</section>
 					</div>
 				</div>
-				<div className="col-12 mb-5 pb-5">
-					<section className="text-center row-extension-money">
+				<div className="col-12 mb-5 pb-md-5">
+					<section className="text-center row-extension-money p-extension">
 						<div className="clic-title ext-title ext-clic-title">
 							<p className="mb-0">Dinero de vuelta</p>
 							<p className="pl-md-2 mb-0">en tus compras diarias</p>
@@ -92,11 +94,13 @@ const ExtensionChrome = () => {
 							<img src={icon_google} alt="Icono Google Chrome" />
 							<button type="button" className="btn btn-blue btn-extension">instalar extensión</button>
 						</div>
-						<div className="extension-detail mt-5">
-							<p className="store-h4 mt-5 text-md-center mb-4">Ahora será mucho más rápido y fácil quedarte con tu cashback en cada compra! Si tienes alguna duda, puedes escribirnos a teayudamos@iwanacash.com.</p>
-							<p className="store-h4 text-md-center">Al instalar la extensión, acepta los términos y condiciones adicionales del botón Cashback, que puede encontrar en los Términos & Condiciones al registrarse en IWANACASH o en la sección  "Ayuda IWANACASH".</p>
-						</div>
 					</section>
+				</div>
+				<div className="col-12">
+					<div className="extension-detail mt-5">
+						<p className="store-h4 mt-5 text-md-center mb-4">Ahora será mucho más rápido y fácil quedarte con tu cashback en cada compra! Si tienes alguna duda, puedes escribirnos a teayudamos@iwanacash.com.</p>
+						<p className="store-h4 text-md-center">Al instalar la extensión, acepta los términos y condiciones adicionales del botón Cashback, que puede encontrar en los Términos & Condiciones al registrarse en IWANACASH o en la sección  "Ayuda IWANACASH".</p>
+					</div>
 				</div>
 			</div>
 		</Layout>
